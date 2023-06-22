72402ffb0ae5e8b1d8e02600016e313bbc11f69d
Austine <augustrush.ae@gmail.com>
2023-06-22 18:45:03 +0100

    cores

--- Cors/coresOptions.js
+++ Cors/coresOptions.js
@@ -1,9 +1,7 @@
 const whiteList = [
-  "http://127.0.0.1:5173/",
+  "http://127.0.0.1:5173",
   "http://localhost:3000",
   "http://localhost",
-  "http://0.0.0.0",
-  "http://127.0.0.1:5173",
 ];
 
 //cors options
