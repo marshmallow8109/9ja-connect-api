954531b530b2b95db8949fd2011a2100890361a4
Austine <augustrush.ae@gmail.com>
2023-07-17 12:11:11 +0100

    c

--- App.js
+++ App.js
@@ -22,14 +22,14 @@ const post = require("./model/post");
 
 app.use(cors());
 //app.set("trust proxy", 1);
-app.use(
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  })
-);
+// app.use(
+//   rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+//   })
+// );
 
 //static middleware
 app.use(express.json());
