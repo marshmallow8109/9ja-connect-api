6665c9d0343de2a8f4ac1614f23fff5b276259f5
Austine <augustrush.ae@gmail.com>
2023-07-17 11:53:16 +0100

    c

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ app.use(
 //static middleware
 app.use(express.json());
 app.use(helmet());
-//app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
